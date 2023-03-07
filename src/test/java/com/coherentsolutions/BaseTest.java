a3e33240d5dafdff10ac700c234077a1cf7f7260
sergeyzubets <zubetssa@gmail.com>
2023-03-07 18:57:17 +0100

    Task 07-Allure report fixes: 1. allureEnvironmentWriter was added to populate environment section.

--- src/test/java/com/coherentsolutions/BaseTest.java
+++ src/test/java/com/coherentsolutions/BaseTest.java
@@ -2,18 +2,35 @@ package com.coherentsolutions;
 
 import com.coherentsolutions.listener.Listener;
 import com.coherentsolutions.pages.MainPage;
+import com.google.common.collect.ImmutableMap;
 import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Listeners;
 
 import static com.coherentsolutions.driver.Driver.getDriverInstance;
 import static com.coherentsolutions.utilities.GetPropertyValues.getPropertyValue;
+import static com.github.automatedowl.tools.AllureEnvironmentWriter.allureEnvironmentWriter;
 
 @Slf4j
 @Listeners({Listener.class})
 public class BaseTest {
     protected WebDriver driver;
+
+    @BeforeSuite
+    void setAllureEnvironment() {
+        Capabilities cap = ((RemoteWebDriver) getDriverInstance().getDriver()).getCapabilities();
+        allureEnvironmentWriter(
+                ImmutableMap.<String, String>builder()
+                        .put("OS", cap.getPlatformName().toString())
+                        .put("Browser", cap.getBrowserName().toLowerCase())
+                        .put("Browser.Version", cap.getBrowserVersion())
+                        .put("URL", getPropertyValue("loginPageUrl"))
+                        .build());
+    }
 
     @AfterSuite
     public void tearDown() {
--- src/test/java/com/coherentsolutions/listener/Listener.java
+++ src/test/java/com/coherentsolutions/listener/Listener.java
@@ -3,9 +3,7 @@ package com.coherentsolutions.listener;
 import com.coherentsolutions.driver.Driver;
 import io.qameta.allure.Attachment;
 import lombok.extern.slf4j.Slf4j;
-import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.IInvokedMethod;
 import org.testng.IInvokedMethodListener;
 import org.testng.ITestContext;
@@ -21,23 +19,11 @@ public class Listener implements IInvokedMethodListener {
         return makeScreenshot(driver);
     }
 
-    @Attachment(value = "Environment details", type = "text/plain")
-    public String saveErrorLog(WebDriver driver) {
-        Capabilities cap = ((RemoteWebDriver) driver).getCapabilities();
-        String browserName = cap.getBrowserName().toLowerCase();
-        String os = cap.getPlatformName().toString();
-        String version = cap.getBrowserVersion();
-        return "Browser.name = " + browserName + "\n" +
-                "Browser.version = " + version + "\n" +
-                "OS = " + os;
-    }
-
     @Override
     public void afterInvocation(IInvokedMethod method, ITestResult testResult, ITestContext context) {
         if (testResult.getStatus() == ITestResult.FAILURE) {
             WebDriver driver = Driver.getDriverInstance().getDriver();
             saveScreenshot(driver);
-            saveErrorLog(driver);
         }
     }
 }
